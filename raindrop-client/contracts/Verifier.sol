74937b474005445a67ae5f79f010285bb42e34e5
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-03-31 16:42:03 -0400

    updates verifier logic
    forces unofficial application names to lowercase
    
    small changes to applicationNameTaken
    
    tests updated accordingly

--- raindrop-client/contracts/Verifier.sol
+++ raindrop-client/contracts/Verifier.sol
@@ -1,31 +1,19 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.21;
 
 import "./zeppelin/ownership/Ownable.sol";
 
-/* pragma solidity ^0.4.11;
-
-contract StringToLower {
-	function _toLower(string str) internal returns (string) {
-		bytes memory bStr = bytes(str);
-		bytes memory bLower = new bytes(bStr.length);
-		for (uint i = 0; i < bStr.length; i++) {
-			// Uppercase character...
-			if ((bStr[i] >= 65) && (bStr[i] <= 90)) {
-				// So we add 32 to make it lowercase
-				bLower[i] = bytes1(int(bStr[i]) + 32);
-			} else {
-				bLower[i] = bStr[i];
-			}
-		}
-		return string(bLower);
-	}
-} */
+contract ERC20Basic {
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+}
 
 contract Verifier is Ownable {
-    // Event for when a user signs up for Hydro 2FA
-    event UserSignUp(bytes32 indexed userNameHash, string userName, bool official);
-    // Event for when an application signs up for Hydro 2FA
-    event ApplicationSignUp(bytes32 indexed applicationNameHash, string applicationName, bool official);
+    // Event for when a user signs up for Hydro 2FA / deletes their account
+    event UserSignUp(string userName, address userAddress, bool official);
+    event UserDeleted(string userName, address userAddress, bool official);
+    // Event for when an application signs up for Hydro 2FA / deletes their account
+    event ApplicationSignUp(string applicationName, bool official);
+    event ApplicationDeleted(string applicationName, bool official);
 
     uint public unofficialUserSignUpFee;
     uint public unofficialApplicationSignUpFee;
@@ -47,11 +35,7 @@ contract Verifier is Ownable {
     mapping (bytes32 => Application) officialApplicationDirectory;
     mapping (bytes32 => Application) unofficialApplicationDirectory;
 
-    function deleteUser(string userName) public onlyOwner {
-        require(userNameTaken(userName));
-        bytes32 userNameHash = keccak256(userName);
-        delete userDirectory[userNameHash];
-    }
+    function Verifier() public {}
 
     // these *Taken functions are redundant in that we have to hash names twice
     // but they're useful to call as public functions
@@ -60,23 +44,68 @@ contract Verifier is Ownable {
         return userDirectory[userNameHash]._initialized;
     }
 
-    function applicationNameTaken(string applicationName) public view returns (bool officialTaken, bool unofficialTaken) {
+    function applicationNameTaken(string applicationName, bool official) public view returns (bool taken) {
         bytes32 applicationNameHash = keccak256(applicationName);
-        return (
-            officialApplicationDirectory[applicationNameHash]._initialized,
-            unofficialApplicationDirectory[applicationNameHash]._initialized
-        );
+        if (official) {
+            return officialApplicationDirectory[applicationNameHash]._initialized;
+        } else {
+            return unofficialApplicationDirectory[applicationNameHash]._initialized;
+        }
     }
 
-    function Verifier() public {}
+    // app users must sign "Delete" with their public key for the Hydro API to be able to delete their account
+    function deleteUserForUser(string userName, uint8 v, bytes32 r, bytes32 s) public onlyOwner {
+        require(userNameTaken(userName));
+        bytes32 userNameHash = keccak256(userName);
+        address userAddress = userDirectory[userNameHash].userAddress;
+        require(isSigned(userAddress, keccak256("Delete"), v, r, s));
+
+        delete userDirectory[userNameHash];
+
+        emit UserDeleted(userName, userAddress, true);
+
+    }
+
+    // alternatively, users can can call this function from their registered address
+    function deleteUser(string userName) public {
+        require(userNameTaken(userName));
+        bytes32 userNameHash = keccak256(userName);
+        require(userDirectory[userNameHash].userAddress == msg.sender);
+        address userAddress = userDirectory[userNameHash].userAddress;
+
+        delete userDirectory[userNameHash];
+
+        emit UserDeleted(userName, userAddress, true);
+    }
+
+    function deleteApplication(string applicationName, bool official) public onlyOwner {
+        require(applicationNameTaken(applicationName, official));
+        bytes32 applicationNameHash = keccak256(applicationName);
+        if (official) {
+            delete officialApplicationDirectory[applicationNameHash];
+        } else {
+            delete unofficialApplicationDirectory[applicationNameHash];
+        }
+    }
+
+    function allLower(string memory _string) public pure returns (bool) {
+        bytes memory bytesString = bytes(_string);
+        for (uint i = 0; i < bytesString.length; i++) {
+			// Uppercase characters
+            if ((bytesString[i] >= 65) && (bytesString[i] <= 90)) {
+                return false;
+			}
+        }
+        return true;
+    }
 
     // called exclusively by the Hydro API when users sign up through the app
-    function officialUserSignUp(string userName, address userAddress) public onlyOwner returns (bytes32 userNameHash) {
-        return _userSignUp(userName, userAddress, true);
+    function officialUserSignUp(string userName, address userAddress) public onlyOwner {
+        _userSignUp(userName, userAddress, true);
     }
 
     // called by individuals who want to create their own account
-    function unofficialUserSignUp(string userName) public payable returns (bytes32 userNameHash) {
+    function unofficialUserSignUp(string userName) public payable {
         require(msg.value >= unofficialUserSignUpFee);
         require(bytes(userName).length < 100);
 
@@ -84,41 +113,36 @@ contract Verifier is Ownable {
     }
 
     // common logic for all user signups regardless of origin
-    function _userSignUp(string userName, address userAddress, bool official) internal returns (bytes32 userNameHash) {
+    function _userSignUp(string userName, address userAddress, bool official) internal {
         require(!userNameTaken(userName));
         bytes32 _userNameHash = keccak256(userName);
         userDirectory[_userNameHash] = User(userName, userAddress, official, true);
 
-        UserSignUp(_userNameHash, userName, official);
-
-        return _userNameHash;
+        emit UserSignUp(userName, userAddress, official);
     }
 
     // called exclusively by the Hydro API when official applications are added
-    function officialApplicationSignUp(string applicationName) public onlyOwner returns (bytes32 applicationNameHash) {
-        var (nameTaken, ) = applicationNameTaken(applicationName);
-        require(!nameTaken);
+    function officialApplicationSignUp(string applicationName) public onlyOwner {
+        bool officialNameTaken = applicationNameTaken(applicationName, true);
+        require(!officialNameTaken);
         bytes32 _applicationNameHash = keccak256(applicationName);
         officialApplicationDirectory[_applicationNameHash] = Application(applicationName, true, true);
 
-        ApplicationSignUp(_applicationNameHash, applicationName, true);
-
-        return _applicationNameHash;
+        emit ApplicationSignUp(applicationName, true);
     }
 
-    // called by applications that want to create their own account
-    function unofficialApplicationSignUp(string applicationName) public payable returns (bytes32 applicationNameHash) {
+    // called by applications that want to create their own account (must be all lowercase)
+    function unofficialApplicationSignUp(string applicationName) public payable {
         require(msg.value >= unofficialApplicationSignUpFee);
         require(bytes(applicationName).length < 100);
 
-        var (, nameTaken) = applicationNameTaken(applicationName);
-        require(!nameTaken);
+        require(allLower(applicationName));
+        require(!applicationNameTaken(applicationName, false));
+
         bytes32 _applicationNameHash = keccak256(applicationName);
         unofficialApplicationDirectory[_applicationNameHash] = Application(applicationName, false, true);
 
-        ApplicationSignUp(_applicationNameHash, applicationName, false);
-
-        return _applicationNameHash;
+        emit ApplicationSignUp(applicationName, false);
     }
 
     function recoverAddress(bytes32 messageHash, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
@@ -144,9 +168,13 @@ contract Verifier is Ownable {
         unofficialApplicationSignUpFee = newFee;
     }
 
-    // add withdrawToken functionality?
-    // require the payment to be in HYDRO rather than ETH?
-    function withdraw(address to) public onlyOwner {
-        to.transfer(this.balance);
+    function withdrawEther(address to) public onlyOwner {
+        to.transfer(address(this).balance);
+    }
+
+    // safety valve ensuring that no ERC-20 token can ever be "stuck" in the contract
+    function withdrawToken(address tokenAddress, address to) public onlyOwner {
+        ERC20Basic token = ERC20Basic(tokenAddress);
+        token.transfer(to, token.balanceOf(address(this)));
     }
 }
