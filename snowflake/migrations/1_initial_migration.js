aca40c48b2e732e67a985660af8ac22a5a234ea0
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-11-29 21:31:34 -0500

    fix migrations

--- snowflake/migrations/1_initial_migration.js
+++ snowflake/migrations/1_initial_migration.js
@@ -5,7 +5,7 @@ const HydroToken = artifacts.require('./_testing/HydroToken.sol')
 
 const SafeMath = artifacts.require('./zeppelin/math/SafeMath.sol')
 const Snowflake = artifacts.require('./Snowflake.sol')
-const Status = artifacts.require('./resolvers/Status.sol')
+// const Status = artifacts.require('./resolvers/Status.sol')
 
 const StringUtils = artifacts.require('./resolvers/ClientRaindrop/StringUtils.sol')
 const ClientRaindrop = artifacts.require('./resolvers/ClientRaindrop/ClientRaindrop.sol')
@@ -25,8 +25,8 @@ module.exports = async function (deployer) {
 
   // const identityRegistry = await deployer.deploy(IdentityRegistry)
   // const hydroToken = await deployer.deploy(HydroToken)
-  const snowflake = await deployer.deploy(Snowflake, '0xDeE6120D632007AC29a720372e943Ae8ED7A783d', '0x4959c7f62051D6b2ed6EaeD3AAeE1F961B145F20')
-  // // const oldClientRaindrop = await deployer.deploy(OldClientRaindrop)
-  await deployer.deploy(ClientRaindrop, snowflake.address, '0xb29778Cf8abFFF8BF245b9060CD2299ADb358040', 0, 0)
-  await deployer.deploy(Status, snowflake.address)
+  // const snowflake = await deployer.deploy(Snowflake, identityRegistry.address, hydroToken.address)
+  // const oldClientRaindrop = await deployer.deploy(OldClientRaindrop)
+  // await deployer.deploy(ClientRaindrop, snowflake.address, oldClientRaindrop.address, 0, 0)
+  // await deployer.deploy(Status, snowflake.address)
 }
